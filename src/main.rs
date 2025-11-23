2bccf51040663dbb1836e8d48f8f120ff20f1029
Roman Nikolaenkov <romanliveshere@gmail.com>
2025-11-23 09:26:59 -0800

    set up minimal bracket lib window

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
+use bracket_lib::prelude::*;
+
 enum GameMode {
     Menu,
     Playing,
     End,
 }
 
-fn main() {
-    println!("Hello, world!");
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.print(1, 1, "Hello Bracket World");
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Hello Minimal Bracket World")
+        .build()?;
+
+    let gs: State = State {};
+    main_loop(context, gs)
 }
